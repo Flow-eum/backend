d22e8fc4b44d72edccc6d3d6d2fc48885c31d744
LYJ0304 <omlmg0304@naver.com>
2025-11-19 15:21:31 +0900

    Initial Commit

---
+++ src/main/java/com/flow/eum_backend/EumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flow.eum_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flow/eum_backend/EumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.eum_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
